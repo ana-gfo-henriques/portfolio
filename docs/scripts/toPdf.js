33d30ed19a9d3600e1921e32ced7c92c17ee6da4
ana-gfo-henriques <ana.gfo.henriques@gmail.com>
2023-03-14 18:15:15 +0000

    retrying

--- docs/scripts/toPdf.js
+++ docs/scripts/toPdf.js
@@ -1,38 +1,26 @@
-// const page = document.getElementById("body");
+import {jsPDF} from "https://cdn.skypack.dev/jspdf";
+// import jspdfAutotable from 'https://cdn.skypack.dev/jspdf-autotable';
 
-// console.log("before doc");
+const page = document.getElementById("body");
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     const doc = new jsPDF({
-//         orientation: "landscape",
-//         unit: "mm",
-//         size: "a4"
-//     });
-//     doc.html(page, {
-//         callback: function(doc) {
-//             doc.save('sample-document.pdf');
-//         },
-//         x: 0,
-//         y: 0,
-//         width: 297,
-//         height: 210
-//     });
+console.log("before doc");
 
-//     console.log("after doc");
+document.addEventListener('DOMContentLoaded', function() {
+    const doc = new jsPDF({
+        orientation: "landscape",
+        unit: "mm",
+        size: "a4"
+    });
+    doc.html(page, {
+        callback: function(doc) {
+            doc.save('sample-document.pdf');
+        },
+        x: 0,
+        y: 0,
+        width: 297,
+        height: 210
+    });
 
-//  }, false);
+    console.log("after doc");
 
-
-var button = document.getElementById('btnDownload');
-
-function generatePDF(){
-  var doc = new jsPDF();
-
-  doc.setFontSize(14);
-  doc.text(20, 20, content.value);
-  //doc.text(35, 25, "Paranyan loves jsPDF");
-  //doc.addImage(imgData, 'JPEG', 15, 40, 180, 160);
-  doc.save('my.pdf');
-}
-
-button.addEventListener('click', generatePDF);
+ }, false);
