5526dc1bd5d27a00c102048687e8b1bd3cc0dbe9
ana-gfo-henriques <ana.gfo.henriques@gmail.com>
2023-03-15 16:58:05 +0000

    and again

--- docs/scripts/canvas2image.js
+++ docs/scripts/canvas2image.js
@@ -206,10 +206,10 @@ var Canvas2Image = function () {
 	 * @return {[type]}          [description]
 	 */
 	var saveAsImage = function (canvas, width, height, type, filename) {
+        console.log(canvas);
 		if ($support.canvas && $support.dataURL) {
 			if (typeof canvas == "string") { canvas = document.getElementById(canvas); }
 			if (type == undefined) { type = 'png'; }
-            if (type == 'jpeg') { type = 'jpg'; }
 			filename = filename == undefined||filename.length === 0 ?Date.now()+'.'+type: filename+'.'+type
 			type = fixType(type);
 
