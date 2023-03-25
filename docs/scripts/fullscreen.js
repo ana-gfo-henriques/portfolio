f88184c1a1d5d543bfc31ced9625b6fc8d6ce841
ana-gfo-henriques <ana.gfo.henriques@gmail.com>
2023-03-25 01:11:01 +0000

    more changes

---
+++ docs/scripts/fullscreen.js
@@ -0,0 +1,25 @@
+function fullscreen(imgId) {
+
+    var imgSrc = document.getElementById(imgId).getAttribute("src");
+    var fullscreenImage = document.getElementById("fullscreenImage");
+
+    var overlay = document.getElementById("overlay");
+    var bodyWidth = document.getElementById('body').clientWidth;
+    var bodyHeight = document.getElementById('body').clientHeight;
+
+    var imgUrl = "url('" + imgSrc + "')";
+
+    console.log(imgSrc);
+    console.log(imgUrl);
+    
+    if (overlay.style.display === "block") {
+        overlay.style.display = "none";
+    } 
+    else {
+        overlay.style.display = "block";
+
+        overlay.style.width = bodyWidth.toString() + "px";
+        overlay.style.height = bodyHeight.toString() + "px";
+        fullscreenImage.style.backgroundImage = imgUrl;
+    } 
+}
