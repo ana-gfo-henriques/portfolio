13a0b7848431883f46a4ef85f1fd9ca09b7e2054
ana-gfo-henriques <ana.gfo.henriques@gmail.com>
2025-03-27 18:47:33 +0000

    Added BD 2025 E-vite

---
+++ docs/dialogs_trials/sketch.js
@@ -0,0 +1,53 @@
+// ml5.js: Pose Estimation with PoseNet
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/Courses/ml5-beginners-guide/7.1-posenet.html
+// https://youtu.be/OIo-DIOkNVg
+// https://editor.p5js.org/codingtrain/sketches/ULA97pJXR
+
+let video;
+let poseNet;
+let pose;
+//let skeleton;
+
+var mySvg; 
+
+// function preload(){
+// 	mySvg = loadImage("shapes.svg");
+// }
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(poses) {
+  //console.log(poses); 
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
+  }
+}
+
+
+function modelLoaded() {
+  console.log('poseNet ready');
+}
+
+function draw() {
+  image(video, 0, 0, width, height, 0);
+
+  if (pose) {
+    
+    for (let i = 0; i < 5; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      fill(0,255,0);
+      ellipse(x,y,160,160);
+    //   image(mySvg, 0, 0);
+    }
+    
+  }
+}
